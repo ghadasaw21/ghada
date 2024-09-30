fb5fc18138f28577233a2fd068e2f2dd697168fc
ghadasaw21 <academyghadaphone@icloud.com>
2024-09-30 14:42:39 +0300

    Initial Commit

---
+++ ghada/ghadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghadaApp.swift
+//  ghada
+//
+//  Created by Ghada al ajmi on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ghadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
